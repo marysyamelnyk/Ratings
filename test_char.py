8161b47c77d98b7bd9b52bca3e8cd94281ae3a46
Maryna Melnyk <marynka555@gmail.com>
2025-04-30 16:59:30 +0300

    char tests done

--- test_char.py
+++ test_char.py
@@ -1,3 +1,4 @@
+import datetime
 from flask.testing import FlaskClient
 from werkzeug.security import generate_password_hash
 import pytest
@@ -21,21 +22,17 @@ def client():
             db.drop_all()
 
 @pytest.fixture
-def create_user(client: FlaskClient):
-    user = User(
-        username="testuser",
-        email="test@example.com",
-        password=generate_password_hash("securepass123"),
-        telegram_id=123456789
-    )
+def create_user(client):
     with client.application.app_context():
+        user = User(
+            username="testuser",
+            email="test@example.com",
+            password=generate_password_hash("securepass123"),
+            telegram_id=123456789
+        )
         db.session.add(user)
         db.session.commit()
-
-    with client.application.app_context():
-        login_user(user)
-    
-    return user
+        return user
 
 @pytest.fixture
 def create_custom_user(client: FlaskClient):
@@ -54,54 +51,77 @@ def create_custom_user(client: FlaskClient):
 
 @pytest.fixture
 def create_parsing_results(client: FlaskClient):
-    result_1 = ParsingResult(
-        user_email="test@example.com",
-        url="http://example1.com",
-        xpath="//div[@id='test1']",
-        result="Test Result 1",
-        timestamp="2025-04-29 12:00:00"
-    )
-    result_2 = ParsingResult(
-        user_email="test@example.com",
-        url="http://example2.com",
-        xpath="//div[@id='test2']",
-        result="Test Result 2",
-        timestamp="2025-04-29 12:05:00"
-    )
     with client.application.app_context():
-        db.session.add(result_1)
-        db.session.add(result_2)
+        result_1 = ParsingResult(
+            user_email="test@example.com",
+            url="http://example1.com",
+            xpath="//div[@id='test1']",
+            result="Test Result 1",
+            timestamp=datetime.datetime(2025, 4, 29, 12, 0, 0),
+        )
+        result_2 = ParsingResult(
+            user_email="test@example.com",
+            url="http://example2.com",
+            xpath="//div[@id='test2']",
+            result="Test Result 2",
+            timestamp=datetime.datetime(2025, 4, 29, 12, 0, 0),
+        )
+
+        db.session.add_all([result_1, result_2])
         db.session.commit()
 
-    return [result_1, result_2]
+        db.session.refresh(result_1)
+        db.session.refresh(result_2)
+
+        return [result_1, result_2]
 
 def test_register_success(client: FlaskClient):
-    data = {
-        "username": "testuser",
-        "email": "test@example.com",
-        "password": "securepass123"
-    }
-    response = client.post("/register", data=data, follow_redirects=True)
-    assert b"Registration successful" in response.data
+    with client.application.app_context():
+        data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "securepass123"
+        }
+        response = client.post("/register", data=data, follow_redirects=False)  # Без follow_redirects
+
+        print(response.data)  # Вивести відповідь для перевірки
+        assert response.status_code == 302  # Перевірка статусного коду редіректу
+        assert response.location.endswith("/login")  # Перевірка редіректу на сторінку логіну
 
 def test_login_success(client: FlaskClient, create_user: User):
-    data = {
-        "email": "test@example.com",
-        "password": "securepass123",
-        "remember": "",
-    }
-    response = client.post("/login", data=data, follow_redirects=True)
-    assert b"Login successful" in response.data
+    with client.application.app_context():
+        data = {
+            "email": "test@example.com",
+            "password": "securepass123",
+        }
+        response = client.post("/login", data=data, follow_redirects=False)  # Без follow_redirects для перевірки редіректу
+
+        assert response.status_code == 302
+        assert response.location.endswith("/parse")
 
 def test_delete_profile_success(client: FlaskClient, create_user: User):
-    response = client.post('/delete_profile', follow_redirects=True)
-    assert b"Your profile has been deleted successfully" in response.data
+    login_data = {
+        "email": "test@example.com",
+        "password": "securepass123",
+    }
+    client.post("/login", data=login_data, follow_redirects=True)
+
+    response = client.post('/delete_profile', follow_redirects=False)
+
+    assert response.status_code == 302
+    assert response.location.endswith("/register")
 
     with client.application.app_context():
         user_check = User.query.filter_by(email="test@example.com").first()
         assert user_check is None
 
 def test_results(client: FlaskClient, create_user: User, create_parsing_results: list[ParsingResult]):
+    login_data = {
+        "email": "test@example.com",
+        "password": "securepass123",
+    }
+    client.post("/login", data=login_data, follow_redirects=True)
+
     response = client.get("/results")
     
     assert response.status_code == 200
@@ -111,6 +131,12 @@ def test_results(client: FlaskClient, create_user: User, create_parsing_results:
     assert json_data[1]["xpath"] == create_parsing_results[1].xpath
 
 def test_delete_result_success(client: FlaskClient, create_user, create_parsing_results):
+    login_data = {
+        "email": "test@example.com",
+        "password": "securepass123"
+    }
+    client.post("/login", data=login_data, follow_redirects=True)
+
     result_to_delete = create_parsing_results[0]
     response = client.delete(f"/delete_result?url={result_to_delete.url}", follow_redirects=True)
     assert response.status_code == 200
@@ -121,26 +147,26 @@ def test_delete_result_success(client: FlaskClient, create_user, create_parsing_
         assert deleted_result is None
 
 def test_send_telegram_message_success(client: FlaskClient, create_user: User):
-    with patch("app.utils.bot.send_message") as mock_send:
+    with patch("telegram_bot.bot.send_message") as mock_send:
         result = send_telegram_message("test@example.com", "Hello test")
         assert result is True
-        mock_send.assert_called_once_with(123456789, "Hello test")
+        mock_send.assert_called_once_with("123456789", "Hello test")
 
 def test_send_telegram_message_no_user(client):
-    with patch("app.utils.bot.send_message") as mock_send:
+    with patch("telegram_bot.bot.send_message") as mock_send:
         result = send_telegram_message("nonexistent@example.com", "Hello test")
         assert result is False
         mock_send.assert_not_called()
 
 def test_send_telegram_message_no_telegram_id(client: FlaskClient, create_custom_user):
     create_custom_user("noiduser", "noid@example.com", None)
-    with patch("app.utils.bot.send_message") as mock_send:
+    with patch("telegram_bot.bot.send_message") as mock_send:
         result = send_telegram_message("noid@example.com", "Hello test")
         assert result is False
         mock_send.assert_not_called()
 
 def test_send_telegram_message_exception(client: FlaskClient, create_custom_user):
     create_custom_user("erroruser", "error@example.com", 987654321)
-    with patch("app.utils.bot.send_message", side_effect=Exception("Mocked error")):
+    with patch("telegram_bot.bot.send_message", side_effect=Exception("Mocked error")):
         result = send_telegram_message("error@example.com", "Hello test")
         assert result is False
